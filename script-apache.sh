d65942fc09df2b8b29a65680700730d84e2b3a96
gasilsantos <silvasantosgabriel20@gmail.com>
2023-08-27 17:29:41 -0300

    servidor web

---
+++ script-apache.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+echo "atualizando o servidor..."
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+echo "instalando o apache..."
+
+sudo apt-get install apache2 -y 
+
+echo "instalando a aplicação..."
+
+cd /tmp
+sudo git clone https://github.com/gasilsantos/projeto-cordel.git
+cd projeto-cordel
+sudo cp -R * /var/www/html
+
+echo "fim do código..."
